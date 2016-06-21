618e69367617d1667f351f82879bbaf93abe84bd
Mitch Sharber <mitchkylell@gmail.com>
2016-06-21 09:54:03 -0400

    Added search.php file
    This file will be linked to the main page, when search is clicked, this page
    will load. It will be responsible for housing the search and displaying
    the search results

---
+++ search.php
@@ -0,0 +1,7 @@
+<div class="search">
+		<form method="get" action="search.php">
+			<label for="s">Search:</label>
+			<input type="text" name="s" id="s">
+			<input type="submit" value="go">
+		</form>
+	</div>
