8456fa198e8e3de93259f8f040d1a6155f10d93f
Mitch Sharber <mitchkylell@gmail.com>
2016-06-21 15:57:30 -0400

    Added link from index.html to search.php
    Also added a var_dump to search too see if the search was working and to
    figure out how to display the results.

--- search.php
+++ search.php
@@ -1,3 +1,23 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+ini_set("html_errors", 1);
+include("inc/functions.php");
+
+$search = null;
+
+if (isset($_GET["s"])) {
+	$search = filter_input(INPUT_GET, "s", FILTER_SANITIZE_STRING);
+}
+
+if (!empty($search)) {
+	$intake = search_intakes($search);
+	var_dump($intake);
+}
+
+
+?>
+
 <div class="search">
 		<form method="get" action="search.php">
 			<label for="s">Search:</label>
