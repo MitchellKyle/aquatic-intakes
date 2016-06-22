577f48f02844f83885fefff2090f6a29a1a78239
Mitch Sharber <mitchkylell@gmail.com>
2016-06-22 10:01:54 -0400

    Changed the search function to accept multiple search fields
    Previously the search could only handle the first name, now you can search
    by first name, last name, records number, or date of birth.

--- inc/functions.php
+++ inc/functions.php
@@ -1,17 +1,21 @@
 <?php
 function search_intakes($search) {
-    include("insert.php");
+  include("insert.php");
 
-    try {
-        $sql = "
-            SELECT first_name, last_name,records_number,date_of_birth
-            FROM Intake
-            WHERE first_name
-                LIKE ?
-            ORDER BY 
-            last_name";
+  try {
+    $sql = "
+      SELECT first_name, last_name,records_number,date_of_birth
+      FROM Intake
+      WHERE first_name LIKE ?
+        OR  last_name LIKE ?
+        OR records_number LIKE ?
+        OR date_of_birth LIKE ?
+      ORDER BY last_name";
         $results = $dbh->prepare($sql);
-        $results->bindValue(1,"%".$search."%",PDO::PARAM_STR); 
+        $results->bindValue(1,"%".$search."%",PDO::PARAM_STR);
+        $results->bindValue(2,"%".$search."%",PDO::PARAM_STR);
+        $results->bindValue(3,"%".$search."%",PDO::PARAM_STR);
+        $results->bindValue(4,"%".$search."%",PDO::PARAM_STR);
         $results->execute();
     } catch (Exception $e) {
         echo "Unable to retrieve results";
